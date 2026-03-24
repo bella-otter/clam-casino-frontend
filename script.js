577237003e7d6130f872900fe0d831be7e4ef6a7
bella-otter <tup17855@temple.edu>
2026-03-24 16:43:03 +0900

    connects to address given in text box

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-//pseudocode
+//rough pseudocode:
 //prompt on loading page for server ip
 //new game button: send POST to ip
 //receive game id
+//(NOT YET IMPLEMENTED:) receive 
 //when user clicks a card:
 //send POST request with card info
 //receive response
 //flip card based on response
-//document.getElementById("output").innerHTML = "<h2>hi</h2>";
-alert("hello");
-async function fetchReq() {
-    const response = await fetch("http://127.0.0.1:5000/new",{
+
+const servInput = document.getElementById("servername");
+const form = document.getElementById("serverForm");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const serverID=servInput.value;
+    console.log(serverID);
+    const gamecode = fetchReq(serverID).then(value=>console.log(value));
+});
+
+async function fetchReq(address, jsonToUse=null) {
+    const response = await fetch(address,{
     method:"POST",
     // headers: {
     //    "Content-Type": "application/json"
     //}
-    
+    body: JSON.stringify(jsonToUse)
     })
     const data = await response.text();
     console.log(data);
+    return data;
 }
-fetchReq();
+
+//const gamecode = fetchReq(serverID).then(value=>console.log(value));
+//replace console.log in above with some function that displays the board?
 
 //document.getElementById("output").innerHTML = data;//displaying the info
