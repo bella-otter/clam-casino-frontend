fe9a4be52efe3727354b65ae2616e4adecc1d5d8
bella-otter <tup17855@temple.edu>
2026-03-27 15:26:46 +0900

    gets column totals

--- script.js
+++ script.js
@@ -13,22 +13,34 @@ const form = document.getElementById("serverForm");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     const serverID=servInput.value;
+    const pushAddress=serverID+"/new";//user should only enter address not port or /new
     console.log(serverID);
-    const gamecode = fetchReq(serverID).then(value=>console.log(value));
+    const gamecode = fetchReq(pushAddress).then(value=>connectMain(value, serverID));
 });
+async function connectMain(gamecode, address) {//executes after address form is submitted
+    console.log(gamecode);
+    //send get request for rows/cols
+    //parse that info
+    //display the board accordingly
+    tempAddress = address + "/totals/" + gamecode;
+    console.log(tempAddress);
+    const rowCol= await fetchReq(tempAddress, null, "GET");
+    console.log(rowCol);
+}
 
-async function fetchReq(address, jsonToUse=null) {
+async function fetchReq(address, jsonToUse=null, requestType="POST") {
     const response = await fetch(address,{
-    method:"POST",
+    method:requestType,
     // headers: {
     //    "Content-Type": "application/json"
     //}
-    body: JSON.stringify(jsonToUse)
+    //body: JSON.stringify(jsonToUse)
     })
     const data = await response.text();
     console.log(data);
     return data;
 }
+
 
 //const gamecode = fetchReq(serverID).then(value=>console.log(value));
 //replace console.log in above with some function that displays the board?
